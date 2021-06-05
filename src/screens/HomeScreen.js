9e93bd1402c926f1303dd29e27c6bc9dfd38acbd
Cyril Deschamps <77438408+CyrilDlaPompote@users.noreply.github.com>
2021-06-05 11:55:58 +0200

    Ajout recherche + tendance

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { FlatList, Image, StyleSheet, Text } from 'react-native';
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const HomeScreen = () => {
 
+  const tendance = require('../../customData.json').tendance;
+
   return(
     <SafeAreaView style={styles.container}>
       <Text>Home</Text>
+      <Text>Tendance</Text>
+      <FlatList
+        horizontal
+        keyExtractor={(item) => item.id}
+        data={tendance}
+        renderItem={({item}) =>
+          <Image 
+            style={styles.image} 
+            source={{ uri: item.urlImage }}
+          />
+        }
+      />
+      <Text>Recommendation</Text>
+      <FlatList
+        horizontal
+        keyExtractor={(item) => item.id}
+        data={tendance}
+        renderItem={({item}) =>
+          <Image 
+            style={styles.image} 
+            source={{ uri: item.urlImage }}
+          />
+        }
+      />
     </SafeAreaView>
   );
 };
@@ -22,6 +48,12 @@ const styles = StyleSheet.create({
     flex: 1,
     width: wp(100), 
     height: hp(100),
+  },
+  image: {
+    width: wp(30),
+    height: wp(60),
+    padding: wp(2),
+    margin: wp(2)
   }
 });
 
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,39 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Text, FlatList } from 'react-native';
+import { Input } from 'react-native-elements';
 import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
 
 const SearchScreen = () => {
+  const [text, setText] = useState("");
+  const [data, setData] = useState([]);
+
+  const callApi = async() => {
+    const res = await axios.get(
+      "https://nominatim.openstreetmap.org/",
+      {
+        params: { 
+          street: text, 
+          format: "json",
+          "accept-language": "fr"
+        } 
+      }
+    );
+    const data = res.data.filter(value => value.class == "tourism");
+    setData(data);
+  }
+
   return(
     <View style={styles.container}>
       <Text style={styles.text}>Search</Text>
+      <Input value={text} onChangeText={setText} onSubmitEditing={callApi} />
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.osm_id}
+        renderItem={({item}) =>
+          <Text>{item.display_name}</Text>
+        }
+      />
     </View>
   );
 };
