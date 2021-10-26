f9ec1c8bdd5070f072cae1081ecd80f30275c5d3
Clément Darne <clementdarne8888@gmail.com>
2021-10-26 18:34:22 +0200

    Ajout de la liste des résultats de recherche. - Les éléments peuvent être sélectionnés pour afficher leur détail.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,13 +1,14 @@
 import BacleAPI from '../api/BacleAPI';
 
 import React, { useState } from 'react';
-import { View, StyleSheet, Text, FlatList } from 'react-native';
+import { View, StyleSheet, Text, FlatList, TouchableOpacity } from 'react-native';
 import { Input } from 'react-native-elements';
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import SearchMap from '../components/Map/SearchMap';
+import SearchResultList from '../components/Search/SearchResultList';
 
 
 const SearchScreen = () => {
@@ -15,17 +16,29 @@ const SearchScreen = () => {
 
   const [text, setText] = useState('');
   const [data, setData] = useState([]);
+  const [selectedPlaceIndex, setSelectedPlaceIndex] = useState(-1);
 
   const callSearchAPI = async () => {
     const res = await BacleAPI.search(text);
-    const data = res.data.filter(value => (value.category == "tourism" || value.category == "boundary"));
+    const data = res.data.filter(value => (value.category === "tourism" || value.category === "boundary"));
     
     console.log("--- BACLE SEARCH -----------------------------------");
     console.log(data);
     console.log("----------------------------------------------------");
 
     setData(data);
+
+    // Mise à jour de la sélection de résultat
+    if (data.length === 1)
+      setSelectedPlaceIndex(0);
+    else
+      setSelectedPlaceIndex(-1);
   };
+
+  const selectPlace = (placeIndex) => {
+    setSelectedPlaceIndex(placeIndex);
+    // TODO: Aller à l'emplacement du lieu.
+  } 
 
   return (
     <View style={styles.container}>
@@ -48,32 +61,26 @@ const SearchScreen = () => {
           onSubmitEditing={callSearchAPI} 
           placeholder={"Rechercher"}
         />
-        {/* <FlatList
-          data={data}
-          keyExtractor={item => item.osm_id.toString()}
-          renderItem={({item}) => (
-            <Text>
-              {item.display_name.split(',')[0]} à{' '}
-              {item.display_name.split(',')[4]}
-            </Text>
-          )}
-        /> */}
       </View>
 
       {/* Résultats */}
-      {(data.length > 1)
-      // TODO: Liste de résultats
-        ? null 
+      {/* TODO: Enelever "selectedPlaceIndex === -1" et ajouter un moyen de revenir à la liste après sélection de résultat. */}
+      {(data.length > 1 && selectedPlaceIndex === -1) 
+        ? (
+          <View style={styles.resultArea}>
+            <SearchResultList data={data} onItemPress={(itemIndex) => { selectPlace(itemIndex) }}/>
+          </View>
+        )
         : null
       }
 
       {/* Léger détail du lieu sélectionné */}
-      {(data.length == 1)
+      {(selectedPlaceIndex !== -1)
         ? (
           <View style={styles.infoCard}>
             <Text>
-              {data[0].display_name.split(',')[0]} à
-              {data[0].display_name.split(',')[4]}
+              {data[selectedPlaceIndex].display_name.split(',')[0]} à
+              {data[selectedPlaceIndex].display_name.split(',')[4]}
             </Text>
           </View>
         )
@@ -128,6 +135,24 @@ const styles = StyleSheet.create({
     height: wp(30),
     width: wp(90),
     padding: wp(2),
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
+  },
+  resultArea: {
+    position: 'absolute',
+    bottom: wp(-6),
+    backgroundColor: 'white',
+    borderRadius: wp(4),
+    height: wp(50),
+    width: wp(100),
+    padding: wp(2),
+    paddingBottom: wp(6),
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
