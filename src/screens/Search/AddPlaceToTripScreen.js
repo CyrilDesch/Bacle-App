6f5fb73e2494ba041990ab6151dc4a4178d42411
Clément Darne <clementdarne8888@gmail.com>
2021-12-16 16:38:22 +0100

    Réorganisation des écrans d'ajout d'un lieu à un voyage

--- src/screens/Search/AddPlaceToTripScreen.js
+++ src/screens/Search/AddPlaceToTripScreen.js
@@ -3,6 +3,7 @@ import {Text, View, Button} from 'react-native';
 import {Context as TripContext} from '../../context/TripContext';
 import SelectTrip from '../../components/Trip/SelectTrip';
 
+
 const AddPlaceToTripScreen = ({route, navigation}) => {
   const {state: tripState, getTrips, addPlaceToTrip} = useContext(TripContext);
   const [processState, setProcessState] = useState(0); // 0: en cours, 1: succès, 2: échec
@@ -12,41 +13,62 @@ const AddPlaceToTripScreen = ({route, navigation}) => {
     getTrips();
   }, []);
 
+
+  const selectTrip = () => {
+    return (
+      <SelectTrip
+        trips={tripState.tripList}
+        onSelection={(item, index) => {
+          addPlaceToTrip(tripState.tripList, index, place).then(status => {
+            setProcessState(status);
+          });
+        }}
+      />
+    );
+  }
+
+  const successFeedback = () => {
+    return (
+      <View>
+        <Text>Le lieu a été ajouté avec succès.</Text>
+        <Button
+          title="Ok"
+          onPress={() => {
+            setProcessState(0);
+            navigation.navigate('Search');
+          }}
+        />
+      </View>
+    );
+  };
+
+  const failureFeedback = () => {
+    return (
+      <View>
+        <Text>Le lieu n'a pas pu être ajouté.</Text>
+        <Button title="Réessayer" onPress={() => setProcessState(0)} />
+        <Button
+          title="Annuler"
+          onPress={() => {
+            setProcessState(0);
+            navigation.navigate('Search');
+          }}
+        />
+      </View>
+    );
+  };
+
+
   return tripState.loading ? (
     <Text>Wait</Text>
   ) : processState === 0 ? (
-    <SelectTrip
-      trips={tripState.tripList}
-      onSelection={(item, index) => {
-        addPlaceToTrip(tripState.tripList, index, place).then(status => {
-          setProcessState(status);
-        });
-      }}
-    />
+    selectTrip()
   ) : processState === 1 ? (
-    <View>
-      <Text>Le lieu a été ajouté avec succès.</Text>
-      <Button
-        title="Ok"
-        onPress={() => {
-          setProcessState(0);
-          navigation.navigate('Search');
-        }}
-      />
-    </View>
+    successFeedback()
   ) : (
-    <View>
-      <Text>Le lieu n'a pas pu être ajouté.</Text>
-      <Button title="Réessayer" onPress={() => setProcessState(0)} />
-      <Button
-        title="Annuler"
-        onPress={() => {
-          setProcessState(0);
-          navigation.navigate('Search');
-        }}
-      />
-    </View>
+    failureFeedback()
   );
 };
+
 
 export default AddPlaceToTripScreen;
